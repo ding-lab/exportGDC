26e99ed5b4c1bf7ee19f4990dbced9af572f9065
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2021-01-19 12:57:00 -0600

    Adding scripts for correcting BamMap sample names
    * runs on katmai

--- A_update_sample_names/2_fix_sample_names.storage1.sh
+++ A_update_sample_names/2_fix_sample_names.storage1.sh
@@ -1,6 +1,6 @@
-BAMMAP="/home/mwyczalk_test/Projects/CPTAC3/CPTAC3.catalog/BamMap/katmai.BamMap.dat"
+BAMMAP="/home/mwyczalk_test/Projects/CPTAC3/CPTAC3.catalog/BamMap/storage1.BamMap.dat"
 CATALOG="/home/mwyczalk_test/Projects/CPTAC3/CPTAC3.catalog/CPTAC3.Catalog.dat"
-OUTPUT="dat/katmai.BamMap.20210119.fix_sample_names.dat"
+OUTPUT="dat/storage1.BamMap.20210119.fix_sample_names.dat"
 
 mkdir -p dat
 bash fix_sample_names.sh $BAMMAP $CATALOG > $OUTPUT
