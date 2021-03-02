b8bf524adb413c30008031d1ce561838f89d70f4
Matthew Wyczalkowski <mwyczalk@genome.wustl.edu>
2021-03-02 17:52:09 -0600

    Data delete script updates

--- D_remove_data/10_remove_data.sh
+++ D_remove_data/10_remove_data.sh
@@ -1,7 +1,13 @@
-UUID_DELETE="/gscmnt/gc2521/dinglab/mwyczalk/projects.CPTAC3/export/20210217.globus_storage/E_globus_from_storage1/dat/UUID_to_copy.dat"
-CATALOGD="/home/mwyczalk_test/Projects/CPTAC3/CPTAC3.catalog"
-BM="$CATALOGD/BamMap/katmai.BamMap.dat"
-NEW_BM="dat/katmai.BamMap.post-delete.dat"
+UUID_DELETE="../E_globus_from_storage1/dat/UUID_to_copy.dat"
+
+# MGI
+CATALOGD="/gscuser/mwyczalk/projects/CPTAC3/CPTAC3.catalog"
+ECATALOGD="/home/mwyczalk_test/Projects/CPTAC3/CPTAC3.catalog"
+
+BM="$CATALOGD/BamMap/MGI.BamMap.dat"
+NEW_BM="dat/MGI.BamMap.post-delete.dat"
+
+mkdir -p dat
 
 cat $UUID_DELETE | bash BMUtils/BamMap.rm.sh $@ -v -B $BM - > $NEW_BM
 echo Original BamMap: $BM
